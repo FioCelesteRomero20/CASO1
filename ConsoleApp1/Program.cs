6e93471189a09b4a81244b1bafb43e1b42081db4
PC <PC@DESKTOP-NOE8GP1>
2023-09-02 00:07:38 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese el numero de personas: ");
+int NP = int.Parse(Console.ReadLine());
+float TOT = 0f;
+if (NP > 300)
+{
+    TOT = NP * 75;
+    
+}
+else
+{
+    if (NP > 200)
+    {
+        TOT = NP * 85;
+        
+    }
+    else
+    {
+        TOT = NP * 95;
+        
+    }
+}
+Console.WriteLine("El total es: " + TOT);
